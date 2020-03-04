0a38b17c0cc04e4ce06129debe14cee2256b6482
Joss <carloscorrea.dev@gmail.com>
2020-03-04 12:59:29 -0600

    sdk + basic config

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
-
+var mercadopago = require("mercadopago");
 var app = express();
+
+mercadopago.configure({
+  access_token: process.env.ACCESS_TOKEN
+});
+
+// Crea un objeto de preferencia
+let preference = {
+  items: [
+    {
+      title: "Mi producto",
+      unit_price: 100,
+      quantity: 1
+    }
+  ]
+};
+
+mercadopago.preferences
+  .create(preference)
+  .then(function(response) {
+    // Este valor reemplazará el string "$$init_point$$" en tu HTML
+    global.init_point = response.body.init_point;
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
 
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
